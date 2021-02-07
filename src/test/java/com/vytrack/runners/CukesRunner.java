9be5b49da7d861b88af76b1f25da2b6cb024f2d9
nurullahekin-cybertek <alpekin@gmail.com>
2021-02-07 09:36:31 +0300

    Navigation task

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -7,8 +7,11 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features",
-        glue = "com/vytrack/step_definitions"
+        features = "src/test/resources/features",  //connection with features folder
+        glue = "com/vytrack/step_definitions",     //connection with step definitions folder
+        dryRun = false,
+        tags = "@navigate"
+
 )
 public class CukesRunner {
 }
