8c699f111df7dd7b1b7f3b7eba9127e4f5deb618
nurullahekin-cybertek <alpekin@gmail.com>
2021-02-07 21:47:27 +0300

    Added utilities

--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -1,11 +1,11 @@
 package com.vytrack.step_definitions;
 
-//import com.vytrack.utilities.Driver;
+import com.vytrack.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
-//import org.openqa.selenium.OutputType;
-//import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
 
@@ -16,12 +16,12 @@ public class Hooks {
 
     @After
     public void tearDown(Scenario scenario) {
-//        if(scenario.isFailed()){
-//            final byte[] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
-//            scenario.attach(screenshot,"image/png","screenshot");
-//        }
-//
-//        Driver.closeDriver();
+        if(scenario.isFailed()){
+            final byte[] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png","screenshot");
+        }
+
+        Driver.closeDriver();
 
     }
 
