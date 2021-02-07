b1950f30ec8aa7c1967eee95bc102b74a9cc18a5
nurullahekin-cybertek <alpekin@gmail.com>
2021-02-07 21:27:33 +0300

    Reporting issues

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {"json:target/cucumber.json"},
         features = "src/test/resources/features",  //connection with features folder
         glue = "com/vytrack/step_definitions",     //connection with step definitions folder
         dryRun = false,
